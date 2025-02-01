95bcd413b6aaa81f94df8aa732964989ac72ebb3
tajbaba999 <tajbaba153@gmail.com>
2025-02-01 12:52:23 +0530

    feat: add indian languages constant

---
+++ src/constants/indianLanguages.ts
@@ -0,0 +1,15 @@
+const indianLanguages = [
+  "hi", // Hindi
+  "bn", // Bengali
+  "te", // Telugu
+  "mr", // Marathi
+  "ta", // Tamil
+  "gu", // Gujarati
+  "ur", // Urdu
+  "kn", // Kannada
+  "or", // Odia
+  "ml", // Malayalam
+  "pa", // Punjabi
+];
+
+export default indianLanguages;
