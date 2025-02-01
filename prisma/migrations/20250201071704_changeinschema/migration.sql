bd6a579cb597b90ef8b170fe9ed52fe56995dc3f
tajbaba999 <tajbaba153@gmail.com>
2025-02-01 12:51:30 +0530

    fix: change in schema

---
+++ prisma/migrations/20250201071704_changeinschema/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `translations` on the `FAQ` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "FAQ" DROP COLUMN "translations";
