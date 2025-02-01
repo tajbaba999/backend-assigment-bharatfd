a656819f77c4ba6e93226fc5db8c3c2ffbb146e5
tajbaba999 <tajbaba153@gmail.com>
2025-02-01 11:33:12 +0530

    feat: add supported languages constant

---
+++ src/constants/Lang.ts
@@ -0,0 +1,14 @@
+const languages = [
+  "hi", // Hindi
+  "bn", // Bengali
+  "es", // Spanish
+  "fr", // French
+  "de", // German
+  "zh", // Chinese
+  "ar", // Arabic
+  "ru", // Russian
+  "ja", // Japanese
+  "pt", // Portuguese
+];
+
+export default languages;
